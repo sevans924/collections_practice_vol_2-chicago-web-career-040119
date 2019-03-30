7a5b42d94c67cc4beb559bab2f9df4c851696811
Sarah Elzinga <sevans924@gmail.com>
2019-03-30 15:35:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -51,6 +51,7 @@ def count_elements(array)
     #new_hash = {}
     structure2[0].map do |name, value|
       new_hash = {}
+      binding.pry
       structure1.each do |data|
           #binding.pry
         if structure1[:first_name] == name
