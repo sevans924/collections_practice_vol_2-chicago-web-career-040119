149ca738ebe170283e3176d2490b1ab1bb026082
Sarah Elzinga <sevans924@gmail.com>
2019-03-30 15:29:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,60 @@
-# your code goes here
+def begins_with_r(array)
+first_r = true
+  array.each do |element|
+    if element[0] != "r"
+      first_r = false
+    end
+  end
+  first_r
+end# your code goes here
+
+def contain_a(array)
+  new_array = []
+  array.each do |element|
+    if element.include?("a")
+      new_array << element
+    end
+  end
+  new_array.compact
+end
+
+def first_wa(array)
+  array.each do |element|
+    if element.match?("wa")
+      return element
+    end
+  end
+end
+
+def remove_non_strings(array)
+  array.delete_if do |element|
+    !element.is_a?(String)
+  end
+
+end
+
+def count_elements(array)
+  array.each do |original_hash|
+      original_hash[:count] = 0
+      name = original_hash[:name]
+      array.each do |next_hash|
+        if next_hash[:name] == name
+          original_hash[:count] += 1
+        end
+      end
+    end.uniq
+  end
+
+  def merge_data(structure1, structure2)
+    #new_hash = {}
+    structure2[0].map do |key, value|
+      new_hash = {}
+      structure1.each do |data|
+        if structure1[:first_name] == key
+          new_hash = value.merge(data)
+        end
+      end
+      new_hash
+    end
+
+  end
